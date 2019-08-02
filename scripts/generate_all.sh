8b8df63e792006b01b38d279243f51246e83e0d5
Jaguar0625 <jaguar0625@127.0.0.1>
2019-08-02 15:04:24 -0400

    (scripts) fix shell array passing

--- scripts/generate_all.sh
+++ scripts/generate_all.sh
@@ -10,14 +10,14 @@ fi
 
 # "aggregate/aggregate" tracked by issue #26
 delete=("aggregate/aggregate")
-transaction_inputs_without_aggregate=(${transaction_inputs[@]}/${delete})
+transaction_inputs_without_aggregate=("${transaction_inputs[@]/${delete}}")
 
 builder="$1"
 if [ "$#" -lt 2 ]; then
-	PYTHONPATH=".:${PYTHONPATH}" generate_batch ${transaction_inputs_without_aggregate} "catbuffer" ${builder}
+	PYTHONPATH=".:${PYTHONPATH}" generate_batch transaction_inputs_without_aggregate "catbuffer" ${builder}
 else
 	nis2_root="$2"
 	rm -rf catbuffer/_generated/${builder}
-	PYTHONPATH=".:${PYTHONPATH}" generate_batch ${transaction_inputs_without_aggregate} "catbuffer" ${builder}
+	PYTHONPATH=".:${PYTHONPATH}" generate_batch transaction_inputs_without_aggregate "catbuffer" ${builder}
 	cp catbuffer/_generated/${builder}/* ${nis2_root}/sdk/src/builders/
 fi
